20893ebcbf4b3d529fa0e6aaf236c10b29fa4ed3
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-06 01:34:11 -0700

    Change dark color of header to white

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">I'm Brendan.</h1>
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">I'm Brendan.</h1>
       <p className="text-base md:text-xl mb-3 font-medium">Software Engineer | Web Developer</p>
       <p className="text-sm max-w-xl mb-6 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!<br/><br/>Ex perspiciatis et officiis quod non inventore doloribus repellendus odit dolores porro, vero ut, reprehenderit laudantium{" "}
       <a
