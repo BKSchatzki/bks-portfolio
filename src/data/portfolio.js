140d5481beb0d1715400c693fb362f73bef71cd3
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-05 23:54:42 -0700

    Create PortfolioItem, data file, and boilerplate

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Project 1',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: '#'
+  },
+  {
+    title: 'Project 2',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: '#'
+  },
+  {
+    title: 'Project 3',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: '#'
+  },
+]
