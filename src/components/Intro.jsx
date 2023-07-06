bbe77a140bb8ca53dc2e13add9a69fa56abf81df
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-05 23:18:14 -0700

    Add content and styling to Intro elements

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1>Brendan K. Schatzki</h1>
-      <p>Software Engineer | Web Developer</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae! Ex perspiciatis et officiis quod non inventore doloribus repellendus odit dolores porro, vero ut, reprehenderit laudantium accusantium? Consequatur accusamus quo atque?</p>
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">I'm Brendan.</h1>
+      <p className="text-base md:text-xl mb-3 font-medium">Software Engineer | Web Developer</p>
+      <p className="text-sm max-w-xl mb-6 font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae! Ex perspiciatis et officiis quod non inventore doloribus repellendus odit dolores porro, vero ut, reprehenderit laudantium accusantium? Consequatur accusamus quo atque?</p>
     </div>
   )
 }
