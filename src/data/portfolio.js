4d52656c4d500e81f1f10c2d1e44bd1890f75bd6
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-06 00:07:40 -0700

    Add placeholder pictures to portfolio data

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
     title: 'Project 1',
-    imgUrl: '',
+    imgUrl: 'https://picsum.photos/seed/project1/400/300',
     stack: ['HTML', 'CSS', 'JavaScript'],
     link: '#'
   },
   {
     title: 'Project 2',
-    imgUrl: '',
+    imgUrl: 'https://picsum.photos/seed/project2/400/300',
     stack: ['HTML', 'CSS', 'JavaScript'],
     link: '#'
   },
   {
     title: 'Project 3',
-    imgUrl: '',
+    imgUrl: 'https://picsum.photos/seed/project3/400/300',
     stack: ['HTML', 'CSS', 'JavaScript'],
     link: '#'
   },
