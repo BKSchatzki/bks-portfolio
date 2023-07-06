3d0ef866d3ef3aa8a8ef1d3c2a13ada63d5031bd
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-05 23:14:28 -0700

    Add boilerplate for Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Intro() {
   return (
-    <div>Intro</div>
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1>Brendan K. Schatzki</h1>
+      <p>Software Engineer | Web Developer</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae! Ex perspiciatis et officiis quod non inventore doloribus repellendus odit dolores porro, vero ut, reprehenderit laudantium accusantium? Consequatur accusamus quo atque?</p>
+    </div>
   )
 }
 
