19784197b03efb3d68871b8111267c5c7f50e893
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-06 00:21:38 -0700

    Add TimelineItem and timeline data boilerplate

---
+++ src/components/TimelineItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TimelineItem({year, title, duration, details}) {
+  return (
+    <div>TimelineItem</div>
+  )
+}
+
+export default TimelineItem
---
+++ src/data/timeline.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    year: 'Today',
+    title: 'Title 1',
+    duration: '1 year',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!'
+  },
+  {
+    year: 'Today',
+    title: 'Title 2',
+    duration: '2 year',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!'
+  },
+  {
+    year: 'Today',
+    title: 'Title 3',
+    duration: '3 year',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!'
+  },
+  {
+    year: 'Today',
+    title: 'Title 4',
+    duration: '4 year',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!'
+  },
+  {
+    year: 'Today',
+    title: 'Title 5',
+    duration: '5 year',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!'
+  },
+  {
+    year: 'Today',
+    title: 'Title 6',
+    duration: '6 year',
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem amet vel temporibus possimus modi! Alias iure tenetur possimus amet, voluptatibus recusandae molestias ut tempora exercitationem sit ducimus vitae!'
+  },
+]
