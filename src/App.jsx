0b63637432b920f0378a5a5d8dbfe216fda48ec6
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-05 22:57:02 -0700

    Rewrite App to fix import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Intro from './components/Intro'
 import Portfolio from './components/Portfolio'
-import Timeline from './compoments/Timeline'
+import Timeline from './components/Timeline'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-
+      <Intro/>
+      <Portfolio/>
+      <Timeline/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
