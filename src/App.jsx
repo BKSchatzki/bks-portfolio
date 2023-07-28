f825576baa82e25713554acc1a0043d8f49fc4e6
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-27 20:29:04 -0700

    Move theme switcher to own component and remove redundant padding from button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 import Intro from "./components/Intro";
 import Portfolio from "./components/Portfolio";
 import Timeline from "./components/Timeline";
@@ -28,49 +29,9 @@ function App() {
     }
   }, [theme]);
 
-  const sun = (
-    <svg
-      className="h-6 w-6"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={1.5}
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-
-  const moon = (
-    <svg
-      className="h-6 w-6"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={1.5}
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-
   return (
     <>
-      <button
-        className="fixed right-20 top-4 z-10 rounded-md bg-violet-300 p-1 p-2 text-lg dark:bg-orange-300"
-        onClick={handleThemeSwitch}
-        type="button"
-      >
-        {theme === "dark" ? sun : moon}
-      </button>
+      <ThemeSwitcher handleThemeSwitch={handleThemeSwitch} theme={theme} />
       <div className="min-h-screen bg-white font-inter text-stone-900 dark:bg-stone-900 dark:text-stone-300">
         <div className="mx-auto w-11/12 max-w-5xl">
           <Intro />
